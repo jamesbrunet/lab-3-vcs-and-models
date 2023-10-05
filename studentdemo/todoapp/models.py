deeb05ebf81c49f20ab8628e58bc703a2f47006a
bonesinit <brandonwhite612@gmail.com>
2023-10-05 17:51:11 -0400

    Update models.py
    Working on group and task
    
    + Fixed created_time and last_updated_time

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -1,11 +1,22 @@
 from django.db import models
-from datetime import datetime
+
+class Group(models.Model):
+    groupName = models.CharField(max_length=100)
+    numTasks = Task.
+
+    @property
+    def __tasks__(self):
+        return self.numTasks
+
 
 class Task(models.Model):
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
-    created_time = datetime.now()
-    last_updated_time = datetime.now()
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
+
+
