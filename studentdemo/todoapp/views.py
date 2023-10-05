a73e6b7625a7f0ab9f3993acf3c1e4d6efe8820b
James Brunet <james@brunet.dev>
2023-10-05 02:07:09 +0200

    Add form example

--- studentdemo/todoapp/views.py
+++ studentdemo/todoapp/views.py
@@ -7,3 +7,16 @@ def index(request):
 
     # Insert Model Data into Template, Run Template Code, an Return to Client
     return render(request, 'index.html', {'tasks': tasks})
+
+from .forms import TaskForm
+
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('some_view_name')  # Redirect to wherever you want after saving
+    else:
+        form = TaskForm()
+
+    return render(request, 'template_name.html', {'form': form})
