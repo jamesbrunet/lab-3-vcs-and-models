b6dc2e3086fc1e2834f4bec4ffe0794e7e49a155
Colin Elliott <colinarelliott@gmail.com>
2023-10-05 17:39:56 -0400

    Group and Task backwards, had to remigrate

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+
+    @property
+    def tasks(self):
+        return Task.objects.filter(group=self)
 
 class Task(models.Model):
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     updated_time = models.DateTimeField(auto_now=True)
     created_time = models.DateTimeField(auto_now_add=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
-
-class Group(models.Model):
-    name = models.CharField(max_length=255)
-    tasks = models.ForeignKey(Task, on_delete=models.CASCADE)
-
-    @property
-    def task_count(self):
-        return self.tasks.count()
