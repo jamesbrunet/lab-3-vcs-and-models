9bc22cc08fb07ff10f4925723eba6ca398830c89
James Brunet <james@brunet.dev>
2023-10-05 02:50:57 +0200

    Add code for forms

--- studentdemo/todoapp/views.py
+++ studentdemo/todoapp/views.py
@@ -1,22 +1,20 @@
 from django.http import HttpResponse
 from .models import Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import TaskForm
+
 def index(request):
     # Fetch Data From Model
     tasks = Task.objects.all()
 
-    # Insert Model Data into Template, Run Template Code, an Return to Client
-    return render(request, 'index.html', {'tasks': tasks})
+    # Fetch Form from Forms
+    form = TaskForm()
 
-from .forms import TaskForm
+    # Insert Model Data into Template, Run Template Code, an Return to Client
+    return render(request, 'index.html', {'tasks': tasks, 'form': form})
 
 def create_task(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('some_view_name')  # Redirect to wherever you want after saving
-    else:
-        form = TaskForm()
-
-    return render(request, 'template_name.html', {'form': form})
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('index')  # Redirect to wherever you want after saving
