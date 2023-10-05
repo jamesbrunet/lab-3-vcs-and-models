933039d9af84dec5112bd75a84c06122267a4eb1
XenoSpeaR <chenliangleo20@gmail.com>
2023-10-05 18:45:29 -0400

    liang's created attributs for the models.py

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Task(models.Model):
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+    created_time = models.DateTimeField(default=timezone.now)
+    last_updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.description
