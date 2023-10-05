e6088d6a1e4716882c8064612d7baf45f3a8a88f
Rachel Cheuk Nam Lee <rachelcheuknamlee@cunet.carleton.ca>
2023-10-05 19:09:21 -0400

    updated models.py

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -7,5 +7,14 @@ class Task(models.Model):
     created_time = models.TimeField(auto_now_add=True)
     last_updated_time = models.TimeField(auto_now=True)
 
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    task = models.CharField(max_length=255)
+
+    @property
+    def taskNumber(self):
+
+
+
     def __str__(self):
         return self.description
