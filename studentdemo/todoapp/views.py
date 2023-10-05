ea4757750587a3d783bec57d2a394603217f37f1
bonesinit <brandonwhite612@gmail.com>
2023-10-05 18:05:39 -0400

    Update views.py
    Importing group

--- studentdemo/todoapp/views.py
+++ studentdemo/todoapp/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
-from .models import Task
+from .models import Task, Group
 from django.shortcuts import render
 def index(request):
     # Fetch Data From Model
     tasks = Task.objects.all()
+    groups = Group.objects.all()
 
     # Insert Model Data into Template, Run Template Code, an Return to Client
-    return render(request, 'index.html', {'tasks': tasks})
+    return render(request, 'index.html', {'tasks': tasks}, {'group': groups})
