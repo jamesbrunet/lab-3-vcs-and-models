d5f4258397e99dc3d2260e588b7ca838bd0881b4
bonesinit <brandonwhite612@gmail.com>
2023-10-05 17:08:25 -0400

    UPDATE models.py
    + Added created_time
    + Added last_updated_time

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-
+from datetime import datetime
 
 class Task(models.Model):
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+    created_time = datetime.now()
+    last_updated_time = datetime.now()
 
     def __str__(self):
         return self.description
