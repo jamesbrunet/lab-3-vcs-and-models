da82c8b344813bd15c2fb015ba813e88700a3faa
Xiaoman Lin <xiaomanlin6@gmail.com>
2023-10-06 12:03:01 -0400

    change the model

--- studentdemo/todoapp/models.py
+++ studentdemo/todoapp/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Task(models.Model):
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.description
