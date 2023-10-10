66f944604906ca954ee0ed421dbe0b37bab8116d
James Brunet <james@brunet.dev>
2023-10-10 01:48:24 -0400

    Finish classroom demo

--- studentdemo/studentdemo/settings.py
+++ studentdemo/studentdemo/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- studentdemo/todoapp/views.py
+++ studentdemo/todoapp/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def index(request):
     # Fetch Data From Model
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
 
     # Fetch Form from Forms
     form = TaskForm()
@@ -17,12 +17,14 @@ def index(request):
     # Insert Model Data into Template, Run Template Code, an Return to Client
     return render(request, 'index.html', {'tasks': tasks, 'form': form})
 
-
+@login_required
 @require_POST  # Ensure that only POST requests can access this view
 def create_task(request):
     form = TaskForm(request.POST)
     if form.is_valid():
-        task = form.save()
+        task = form.save(commit=False)
+        task.user = request.user
+        task.save()
         return JsonResponse({'status': 'ok', 'task_description': task.description, 'task_id': task.id})
     else:
         return JsonResponse({'status': 'error', 'errors': form.errors}, status=400)
